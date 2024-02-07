d47d2b719e19e9cc685d33114e22b2e86cf1ed73
lonelytransistor <lonelytransistor@protonmail.com>
2024-02-07 15:11:41 +0100

    Rearrange files for submodule

--- README.md
+++
@@ -1,2 +0,0 @@
-# AndroidCommonlib
-A collection of helpers, classes, resources used in multitude of my projects.
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,32 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.library") version "8.1.3" apply false
+    id("com.android.library")
+}
+
+android {
+    namespace = "net.lonelytransistor.commonlib"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 28
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.leanback:leanback:1.0.0")
 }
